2573ad48817edf8f6e618ffcd195fd875d47bf4a
Faizul Osman <faizulosmanrabby@gmail.com>
2022-11-25 18:09:50 +0600

    products delete operation done

--- index.js
+++ index.js
@@ -179,6 +179,14 @@ async function run() {
       res.send(result);
     });
 
+    // Delete (Product)
+    app.delete("/products/:id", verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productsCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Update sold Status (Products)
     app.put("/products/:id", verifyJWT, async (req, res) => {
       const id = req.params.id;
