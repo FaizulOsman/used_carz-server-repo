80aa45ceed631ddd36009401d067d47dcd436bd4
Faizul Osman <faizulosmanrabby@gmail.com>
2022-11-25 18:12:41 +0600

    users delete operation done

--- index.js
+++ index.js
@@ -129,6 +129,15 @@ async function run() {
       res.send(users);
     });
 
+    // Delete (Product)
+    app.delete("/users/:id", verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // Update user (acting)
     app.put("/users/admin/:id", verifyJWT, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const filter = { _id: ObjectId(id) };
