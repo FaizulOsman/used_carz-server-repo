f7aaed1d6777d7719d76208bd23f53ee930f0fed
Faizul Osman <faizulosmanrabby@gmail.com>
2022-11-26 20:29:19 +0600

    get specific booking

--- index.js
+++ index.js
@@ -115,6 +115,13 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const booking = await bookingsCollection.findOne(query);
+      res.send(booking);
+    });
+
     // Get Admin
     app.get("/users/admin/:email", async (req, res) => {
       const email = req.params.email;
